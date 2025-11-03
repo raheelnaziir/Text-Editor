a37363d73be2fca7cfa04da54a722c8d847595b6
raheelnaziir <raheelnazirc22@gmail.com>
2025-11-03 19:52:37 +0500

    implmented JFrame

--- src/EditorFrame.java
+++ src/EditorFrame.java
@@ -1,4 +1,26 @@
+import java.awt.Color;
+import java.awt.FlowLayout;
 
-public class EditorFrame {
+import javax.swing.JFrame;
+
+public class EditorFrame extends JFrame{
+	
+	int WIDTH = 885;
+	int HEIGHT = 674;
+	
+	EditorFrame() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("  Notepad");
+		this.setBackground(Color.white);
+		this.setSize(900,660);
+		this.getContentPane().setBackground(Color.white);
+		this.setResizable(false);
+		this.setLayout(new FlowLayout());
+		this.setLocationRelativeTo(null);
+		
+		
+		this.setVisible(true);
+		
+	}
 
 }
