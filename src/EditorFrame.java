ac70e80e1ba39d250b6291c986c98c66d120dee3
MRehanMehdi <m.rehanmehdi700@gmail.com>
2025-11-09 20:29:45 +0500

    Add ActionListener implementation and font list setup in EditorFrame

--- src/EditorFrame.java
+++ src/EditorFrame.java
@@ -4,27 +4,36 @@ import java.awt.FlowLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
-public class EditorFrame extends JFrame{
-	
-	int WIDTH = 885;
-	int HEIGHT = 674;
-	
-	ImageIcon icon = new ImageIcon("notepad.png");
-	
-	EditorFrame() {
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("  Notepad");
-		this.setBackground(Color.white);
-		this.setSize(900,660);
-		this.getContentPane().setBackground(Color.white);
-		this.setResizable(false);
-		this.setLayout(new FlowLayout());
-		this.setLocationRelativeTo(null);
-		this.setIconImage(icon.getImage());
-		
-		
-		this.setVisible(true);
-		
-	}
+public class EditorFrame extends JFrame implements ActionListener{
+
+    int WIDTH = 885;
+    int HEIGHT = 674;
+    String []fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+
+    ImageIcon icon = new ImageIcon("notepad.png");
+
+    EditorFrame() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("  Notepad");
+        this.setBackground(Color.white);
+        this.setSize(900,660);
+        this.getContentPane().setBackground(Color.white);
+        this.setResizable(false);
+        this.setLayout(new FlowLayout());
+        this.setLocationRelativeTo(null);
+        this.setIconImage(icon.getImage());
+
+
+        this.setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+
+    }
+
+
 
 }
