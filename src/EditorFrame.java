a0b491f38172eaa791c17e2429bbc32e4d0eed3e
raheelnaziir <raheelnazirc22@gmail.com>
2025-11-24 14:30:29 +0500

    Adding JMenuBar

--- src/EditorFrame.java
+++ src/EditorFrame.java
@@ -7,10 +7,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.undo.UndoManager;
 
 public class EditorFrame extends JFrame implements ActionListener{
 
